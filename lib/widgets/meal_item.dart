5a593546ffe21d962c1d4a3ae8a14d186fcc2d54
Plane <plane2001@outlook.com>
2022-02-06 16:35:43 +0530

    Added meals

---
+++ lib/widgets/meal_item.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+import 'package:meal_dash/models/meal.dart';
+
+class MealItem extends StatelessWidget {
+  final String title;
+  final String imageURL;
+  final int duration;
+  final Complexity complexity;
+  final Affordability affordability;
+
+  MealItem(
+      {required this.title,
+      required this.imageURL,
+      required this.duration,
+      required this.complexity,
+      required this.affordability});
+
+  String get complexityText {
+    switch (complexity) {
+      case Complexity.Simple:
+        return "Simple";
+      case Complexity.Challenging:
+        return "Challenging";
+      case Complexity.Difficult:
+        return "Difficult";
+      default:
+        return "Unknown";
+    }
+  }
+
+  String get affordabilityText {
+    switch (affordability) {
+      case Affordability.Affordable:
+        return "Affordable";
+      case Affordability.Costly:
+        return "Pricey";
+      case Affordability.Luxurious:
+        return "Expensive";
+      default:
+        return "Unknown";
+    }
+  }
+
+  void selectMeal() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: selectMeal,
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
+        elevation: 5,
+        margin: EdgeInsets.all(15),
+        child: Column(
+          children: <Widget>[
+            Stack(
+              children: <Widget>[
+                ClipRRect(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(15),
+                      topRight: Radius.circular(15)),
+                  child: Image.network(
+                    imageURL,
+                    height: 250,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Positioned(
+                  // It allows to position the child at any place inside a stack.
+                  bottom: 20,
+                  right: 10,
+                  child: Container(
+                    width: 300,
+                    color: Colors.black54,
+                    padding: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                    child: Text(
+                      title,
+                      style: TextStyle(fontSize: 25, color: Colors.white),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Expanded(
+                    child: Row(
+                      children: <Widget>[
+                        Icon(Icons.schedule),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Text("$duration min"),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Row(
+                      children: <Widget>[
+                        Icon(Icons.work),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Text(complexityText),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Row(
+                      children: <Widget>[
+                        Icon(Icons.attach_money),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Text(affordabilityText),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
